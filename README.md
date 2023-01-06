9b0c5a99f929a3047d525e0c4542006b8d96a565
Julian Koehn <me@julian.pro>
2023-01-06 11:56:25 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Playground Player
+
+## Build Lifecycle
+
+1. Package
+2. Make
+3. Publish
+
+```bash
+npm run package
+npm run make
+```
---
+++ forge.config.ts
@@ -0,0 +1,51 @@
+import type { ForgeConfig } from "@electron-forge/shared-types";
+import { MakerZIP } from "@electron-forge/maker-zip";
+import { MakerDeb } from "@electron-forge/maker-deb";
+import { MakerRpm } from "@electron-forge/maker-rpm";
+import { WebpackPlugin } from "@electron-forge/plugin-webpack";
+
+import { mainConfig } from "./webpack.main.config";
+import { rendererConfig } from "./webpack.renderer.config";
+
+const config: ForgeConfig = {
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [
+    new MakerZIP({}, ["darwin", "linux"]),
+    new MakerRpm({}),
+    new MakerDeb({}),
+  ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "signageful",
+          name: "client",
+        },
+        prerelease: true,
+      },
+    },
+  ],
+  plugins: [
+    new WebpackPlugin({
+      mainConfig,
+      port: 1337,
+      renderer: {
+        config: rendererConfig,
+        entryPoints: [
+          {
+            html: "./src/index.html",
+            js: "./src/renderer.ts",
+            name: "main_window",
+            preload: {
+              js: "./src/preload.ts",
+            },
+          },
+        ],
+      },
+    }),
+  ],
+};
+
+export default config;
