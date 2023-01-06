61119a8ccdea8ac68e94af76d53ac43ccb24460f
Julian Koehn <me@julian.pro>
2023-01-06 13:26:00 +0100

    linux zip

--- forge.config.ts
+++ forge.config.ts
@@ -10,7 +10,11 @@ import { rendererConfig } from "./webpack.renderer.config";
 const config: ForgeConfig = {
   packagerConfig: {},
   rebuildConfig: {},
-  makers: [new MakerZIP({}, ["darwin"]), new MakerRpm({}), new MakerDeb({})],
+  makers: [
+    new MakerZIP({}, ["darwin", "linux"]),
+    new MakerRpm({}),
+    new MakerDeb({}),
+  ],
   publishers: [
     {
       name: "@electron-forge/publisher-github",
