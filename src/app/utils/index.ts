b83d5057abccc4aee6d6ecd63d10c681a8c5ec40
Julian Koehn <me@julian.pro>
2023-01-26 13:12:25 +0100

    fix: 1.10.2

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -2,7 +2,4 @@ export const isDebug =
   (typeof process !== "undefined" && process.env.NODE_ENV === "development") ||
   (typeof process !== "undefined" && process?.env.DEBUG_PROD === "true");
 
-export const __DEV__ =
-  typeof process == "undefined"
-    ? true
-    : process?.env?.NODE_ENV !== "production";
+export const __DEV__ = false;
